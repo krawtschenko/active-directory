df49d81e9b1a1ffcbb110e2be4739ec4ccad60cf
krawtschenko <eugenykravchenko@gmail.com>
2025-07-11 12:39:12 +0200

    change size

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const { app, BrowserWindow } = require("electron");
+const {app, BrowserWindow} = require("electron");
 const path = require("path");
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1650,
-    height: 1000,
-    icon: path.join(__dirname, "build", "icon.png"),
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+	const mainWindow = new BrowserWindow({
+		width: 1100,
+		height: 820,
+		icon: path.join(__dirname, "build", "icon.png"),
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-  mainWindow.loadFile(path.join(__dirname, "dist", "index.html"));
-  //   mainWindow.webContents.openDevTools();
+	mainWindow.loadFile(path.join(__dirname, "dist", "index.html"));
+	//   mainWindow.webContents.openDevTools();
 
-  // Відключаємо меню
-  mainWindow.removeMenu(); // Викликаємо метод як функцію
+	// Відключаємо меню
+	mainWindow.removeMenu(); // Викликаємо метод як функцію
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
